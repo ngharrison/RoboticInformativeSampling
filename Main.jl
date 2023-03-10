7f46529f81cb5a6e175d5b20b04a25a8215a6221
Nicholas Harrison <nicholasharrison222@gmail.com>
2023-03-10 21:56:58 +1100

    Quick format/organization edit

--- Main.jl
+++ Main.jl
@@ -1,9 +1,11 @@
 # using Debugger
-using Revise
+# using Profile
+# using ProfileView
+# using Logging
+# debuglogger = ConsoleLogger(stderr, Logging.Info)
+# global_logger(debuglogger)
 
-using Logging
-debuglogger = ConsoleLogger(stderr, Logging.Info)
-global_logger(debuglogger)
+using Revise
 
 push!(LOAD_PATH, "./") # allows using modules defined in current directory
 
@@ -18,6 +20,7 @@ gt = createGT()
 weights = [1, 6, 4e-1, 1e-2] # mean, std, dist, prox
 x_start = [0.5, 0.2] # starting location
 
-@time samples, belief_model = explore(region, x_start, weights, gt; show_visuals=false, sleep_time=0);
+@time samples, belief_model = explore(region, x_start, weights, gt;
+                                      show_visuals=false, sleep_time=0.0);
 
 visualize(region, samples, gt, belief_model, weights) # plot stuff
