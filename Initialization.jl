8385be8a05c7ed94f5d92254259adc1547b93b75
Nicholas Harrison <nicholasharrison222@gmail.com>
2023-03-12 22:16:00 +1100

    Update ground-truth creation
    - Extended to have multiple gaussian peaks
    - Future methods for generic data map

--- Initialization.jl
+++ Initialization.jl
@@ -3,35 +3,50 @@ module Initialization
 using LinearAlgebra
 using Distributions
 
-export GT, initializeRegion, createGT
+export GT, GaussGT, Peak, initializeRegion
 
 struct Region
     lb # lower bounds
     ub # upper bounds
 end
 
-# ground truth struct
-struct GT
-    μ # expected value
-    Σ # covariance matrix
-    distr # distribution that generates the values
+abstract type GT end
+
+# ground truth struct for gaussian peaks
+struct GaussGT <: GT
+    peaks
 end
 
-(gt::GT)(X) = pdf(gt.distr, X)./pdf(gt.distr, gt.μ)
-
-function createGT(μ=[0.3, 0.3], Σ=0.03*I)
-    # returns a function that produces ground-truth value(s) for a point or list of points
+function (gt::GaussGT)(X)
+    # produces ground-truth value(s) for a point or list of points
     # accepts a single vector, a vector of vectors, or a matrix of column vectors
-    # μ = [[0.3, 0.3], [0.6, 0.7]]
-    # Σ = [0.03*I, 0.01*I]
-    distr = MvNormal(μ, Σ)
-    return GT(μ, Σ, distr)
+    h_max = maximum(p.h for p in gt.peaks)
+    return sum(p.h*pdf(p.distr, X)/pdf(p.distr, p.distr.μ)/h_max for p in gt.peaks)
 end
+
+struct Peak
+    distr
+    h
+end
+
+Peak(μ, Σ, h) = Peak(MvNormal(μ, Σ), h)
+
+# for future
+# struct GeneralGT <: GT
+#     map
+#     res
+# end
+
+# function (gt::GeneralGT)(X)
+#     # produces ground-truth value(s) for a point or list of points
+#     # accepts a single vector, a vector of vectors, or a matrix of column vectors
+#     indices = [round.(Int, x ./ gt.res).+1 for x in X]
+#     return [gt.map[ind...] for ind in indices]
+# end
 
 function initializeRegion()
     lb = [0, 0]
     ub = [1, 1]
-    # x3 = zeros(n)
     return Region(lb, ub)
 end
 
--- Main.jl
+++ Main.jl
@@ -9,13 +9,17 @@ using Revise
 
 push!(LOAD_PATH, "./") # allows using modules defined in current directory
 
+using LinearAlgebra
 using Initialization
 using Exploration
 using Visualization
 
 
 region = initializeRegion()
-gt = createGT()
+peaks = [Peak([0.3, 0.3], 0.03*I, 1.0),
+         Peak([0.8, 0.7], 0.008*I, 0.4)]
+gt = GaussGT(peaks)
+visualize(region, gt)
 
 weights = [1, 6, 4e-1, 1e-2] # mean, std, dist, prox
 x_start = [0.5, 0.2] # starting location
