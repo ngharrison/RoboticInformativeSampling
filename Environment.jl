b54fa0f7083c2d996e9fa67c0d9a7b9c372e739f
Nicholas Harrison <nicholasharrison222@gmail.com>
2023-04-06 22:23:25 +1000

    Added ability in path graph to move diagonally

--- Environment.jl
+++ Environment.jl
@@ -15,14 +15,37 @@ struct Region
     obsMap # obstacle map
     gtMap # ground truth map
     graph # graph for path finding
+    graph_weights
     vmap # translates vertices in graph
 end
 
 function Region(lb, ub, obsMap, gtMap)
+    # create a graph for path finding
     graph = grid(size(obsMap.data))
-    indices = findall(vec(obsMap.data)) # remove all vertices in collision
+    li = LinearIndices(obsMap.data)
+    m, n = size(obsMap.data)
+    for i in 1:m, j in 1:n
+        i > 1 && j > 1 && add_edge!(graph, li[i,j], li[i-1,j-1])
+        i > 1 && j < n && add_edge!(graph, li[i,j], li[i-1,j+1])
+        i < m && j > 1 && add_edge!(graph, li[i,j], li[i+1,j-1])
+        i < m && j < n && add_edge!(graph, li[i,j], li[i+1,j+1])
+    end
+
+    # give diagonal paths correct distance in weight matrix
+    graph_weights = ones(size(graph))
+    ci = CartesianIndices(obsMap.data)
+    for i in 1:size(graph_weights, 1), j in 1:size(graph_weights, 2)
+        diff = ci[i] - ci[j]
+        if abs(diff[1]) == 1 && abs(diff[2]) == 1
+            graph_weights[i,j] = √2
+        end
+    end
+
+    # remove all vertices in collision
+    indices = findall(vec(obsMap.data))
     vmap = rem_vertices!(graph, indices, keep_order=true)
-    Region(lb, ub, obsMap, gtMap, graph, vmap)
+
+    Region(lb, ub, obsMap, gtMap, graph, graph_weights, vmap)
 end
 
 struct Map
@@ -56,7 +79,7 @@ function pathCost(x1, x2, region)
 
     # calculate cost
     s, t = getGraphIndex.((x1, x2), Ref(region))
-    path = a_star(region.graph, s, t, weights(region.graph), v->norm(t.-v))
+    path = a_star(region.graph, s, t, region.graph_weights, v->norm(t.-v))
     return length(path)*mean(region.obsMap.res)
 end
 
