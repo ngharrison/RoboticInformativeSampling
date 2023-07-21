bfaa83c0cb0cdb1c865a5f6e264c1d21dec7b463
Nicholas Harrison <nicholasharrison222@gmail.com>
2023-07-21 19:32:23 +1000

    fix(DataAnalysis): better handle data loading

--- src/DataAnalysis.jl
+++ src/DataAnalysis.jl
@@ -1,14 +1,24 @@
 # script for analyzing data from missions
 # can be run after Main.jl or by opening a saved file
 
-using Plots
+# allows using modules defined in any file in project src directory
+src_dir = dirname(Base.active_project()) * "/src"
+if src_dir ∉ LOAD_PATH
+    push!(LOAD_PATH, src_dir)
+end
 
-f = jldopen("../output/2023-07-21-17-56-40.jdl2")
+using Missions: Mission
+using BeliefModels: BeliefModel
+using Samples: Sample
+using FileIO: load
+using Plots: plot
 
-mission = f["mission"]
-beliefs = f["beliefs"]
-samples = f["samples"]
-metrics = f["metrics"]
+data = load("../output/2023-07-21-17-56-40.jdl2")
+
+mission = data["mission"]
+beliefs = data["beliefs"]
+samples = data["samples"]
+metrics = data["metrics"]
 
 plot(
     plot(1:mission.num_samples, metrics.mae),
