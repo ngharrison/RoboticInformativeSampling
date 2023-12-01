bdaac62e20ccbbb4d02501a7d79c24855da54b7c
Nicholas Harrison <nicholasharrison222@gmail.com>
2023-12-01 16:37:25 +1100

    refactor(Missions): simplify spatialAve nested loop

--- src/modules/Missions.jl
+++ src/modules/Missions.jl
@@ -462,14 +462,12 @@ function spatialAve(M, extent=1)
     for i in axes(M,1), j in axes(M,2)
         tot = 0
         count = 0
-        for k in -extent:extent
-            for l in -extent:extent
-                m = i + k
-                n = j + l
-                if 1 <= m <= size(M,1) && 1 <= n <= size(M,2) && !isnan(M[m,n])
-                    tot += M[m,n]
-                    count += 1
-                end
+        for k in -extent:extent, l in -extent:extent
+            m = i + k
+            n = j + l
+            if 1 <= m <= size(M,1) && 1 <= n <= size(M,2) && !isnan(M[m,n])
+                tot += M[m,n]
+                count += 1
             end
         end
         N[i,j] = tot/count
