e9e68540dbd636a0a25d312c1094f704d44de3de
Nicholas Harrison <nicholasharrison222@gmail.com>
2024-02-24 21:34:48 +1100

    docs: remove invalid slfm reference

--- src/modules/SLFMKernel.jl
+++ src/modules/SLFMKernel.jl
@@ -7,14 +7,12 @@ Kernel associated with the semiparametric latent factor model.
 
 # Definition
 
-For inputs ``x, x'`` and output dimensions ``p, p''``, the kernel is defined as[^STJ]
+For inputs ``x, x'`` and output dimensions ``p, p''``, the kernel is defined as
 ```math
 k\big((x, p), (x', p')\big) = \sum^{Q}_{q=1} A_{p q}g_q(x, x')A_{p' q},
 ```
 where ``g_1, \ldots, g_Q`` are ``Q`` kernels, one for each latent process, and ``A`` is a matrix of weights for the kernels of
 size ``m \times Q``.
-
-[^STJ]: M. Seeger, Y. Teh, & M. I. Jordan (2005). [Semiparametric Latent Factor Models](https://infoscience.epfl.ch/record/161465/files/slfm-long.pdf).
 """
 struct SLFMMOKernel{Tg,TA<:AbstractMatrix} <: MOKernel
     g::Tg
