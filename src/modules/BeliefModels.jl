3f7f43e3b76ca588bcf5a69f12144e2f2163ffa7
Nicholas Harrison <nicholasharrison222@gmail.com>
2024-03-18 15:30:43 +1100

    misc(BeliefModels): switch to fixed zero noise as default

--- src/modules/BeliefModels.jl
+++ src/modules/BeliefModels.jl
@@ -88,7 +88,7 @@ beliefModel = BeliefModel(samples, M.prior_samples, lb, ub)
 beliefModel = BeliefModel([M.prior_samples; samples], lb, ub)
 ```
 """
-function BeliefModel(samples, prior_samples, lb, ub; σn=1e-3, kernel=multiKernel)
+function BeliefModel(samples, prior_samples, lb, ub; σn=fixed(0.0), kernel=multiKernel)
     # create a simple belief model for the current samples
     current = BeliefModel(samples, lb, ub; σn, kernel)
     isempty(prior_samples) && return current
@@ -107,7 +107,7 @@ conditioned on the samples given.
 A noise standard deviation can optionally be passed in either as a single scalar
 value for all samples or a vector of values, one for each sample.
 """
-function BeliefModel(samples, lb, ub; σn=1e-3, kernel=multiKernel)
+function BeliefModel(samples, lb, ub; σn=fixed(0.0), kernel=multiKernel)
     # set up training data
     X = getfield.(samples, :x)
     Y = getfield.(samples, :y)
@@ -117,7 +117,7 @@ function BeliefModel(samples, lb, ub; σn=1e-3, kernel=multiKernel)
         θ0 = initHyperparams(X, Y_vals, lb, ub; σn=fixed(Y_errs)) # no noise to learn
     else
         Y_vals = Y
-        θ0 = initHyperparams(X, Y_vals, lb, ub; σn=fixed(0.0)) # no noise to learn
+        θ0 = initHyperparams(X, Y_vals, lb, ub; σn) # no noise to learn
     end
 
     # optimize hyperparameters (train)
