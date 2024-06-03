a20d8130f8d91472ce43c0cc8c8dbc84f6e0a405
Nicholas Harrison <nicholasharrison222@gmail.com>
2024-06-03 17:38:46 +1000

    feat: publish maps for all sampled quantities, not just first

--- app/missions/ros_pub_maps.jl
+++ app/missions/ros_pub_maps.jl
@@ -18,6 +18,7 @@ function rosMission(; num_samples=4)
     # the topics that will be listened to for measurements
     data_topics = [
         # Crop height avg and std in frame (excluding wheels)
+        ("value1", "value2"),
         ("value1", "value2")
     ]
 
@@ -66,52 +67,59 @@ rospy = pyimport("rospy")
 std_msg = pyimport("std_msgs.msg")
 sen_msg = pyimport("sensor_msgs.msg")
 
+axs, points = generateAxes(mission.occupancy)
+dims = size(mission.occupancy)
+
+quantities = eachindex(mission.sampler)
+full_indices = [(p, q) for p in points for q in quantities]
+
+# sets up the publishers
 array_publishers, image_publishers = map(("array", "image"),
     (std_msg.Float64MultiArray, sen_msg.Image)) do type_name, type
-    map(("pred", "err")) do name
-        rospy.Publisher("/informative_sampling/$(name)_$(type_name)",
-            type, queue_size=1, latch=true)
+    map(quantities) do q
+        map(("pred", "err")) do name
+            rospy.Publisher("/informative_sampling/$(name)_$(type_name)_$(q)",
+                type, queue_size=1, latch=true)
+        end
     end
 end
 
-axs, points = generateAxes(mission.occupancy)
-dims = size(mission.occupancy)
-quantity = 1
-
 ## run search alg
 @time samples, beliefs = mission() do _, _, beliefModel, _, _
-    μ, σ = beliefModel(tuple.(vec(points), quantity))
+    μ, σ = collect.(Iterators.partition.(beliefModel(full_indices), prod(dims)))
 
-    for (i, data) in enumerate((μ, σ))
-        # publish as array
-        array = std_msg.Float64MultiArray()
-        array.layout.dim = std_msg.MultiArrayDimension.("", collect(dims), 1)
-        array.layout.data_offset = 0
-        array.data = data
-        array_publishers[i].publish(array)
+    for q in quantities
+        for (i, data) in enumerate((μ[q], σ[q]))
+            # publish as array
+            array = std_msg.Float64MultiArray()
+            array.layout.dim = std_msg.MultiArrayDimension.("", collect(dims), 1)
+            array.layout.data_offset = 0
+            array.data = data
+            array_publishers[q][i].publish(array)
 
-        l, h = extrema(data)
-        data_scaled = (data .- l) ./ (h - l + eps())
+            l, h = extrema(data)
+            data_scaled = (data .- l) ./ (h - l + eps())
 
-        # map to image
-        amount = vec(reverse(permutedims(reshape(data_scaled, dims), (2, 1)), dims=1))
+            # map to image
+            amount = vec(reverse(permutedims(reshape(data_scaled, dims), (2, 1)), dims=1))
 
-        # convert to byte values and repeat each 4 times for rgba format
-        byte_data = round.(UInt8, 255 .* amount)
-        img_data = pybytes([x for x in byte_data for _ in 1:4])
+            # convert to byte values and repeat each 4 times for rgba format
+            byte_data = round.(UInt8, 255 .* amount)
+            img_data = pybytes([x for x in byte_data for _ in 1:4])
 
-        env_height, env_width = dims
+            env_height, env_width = dims
 
-        # publish as image
-        image = sen_msg.Image()
-        image.header.stamp = rospy.Time.now()
-        image.header.frame_id = "env_image"
-        image.height = env_height
-        image.width = env_width
-        image.encoding = "rgba8"
-        image.is_bigendian = 1
-        image.step = env_width * 4
-        image.data = img_data
-        image_publishers[i].publish(image)
+            # publish as image
+            image = sen_msg.Image()
+            image.header.stamp = rospy.Time.now()
+            image.header.frame_id = "env_image"
+            image.height = env_height
+            image.width = env_width
+            image.encoding = "rgba8"
+            image.is_bigendian = 1
+            image.step = env_width * 4
+            image.data = img_data
+            image_publishers[q][i].publish(image)
+        end
     end
 end
