786308512bca4e5c927c498c023ea98cb3f76fb8
Nicholas Harrison <nicholasharrison222@gmail.com>
2024-06-11 15:07:17 +1000

    fix(setup): remove docs environment setup

--- setup.jl
+++ setup.jl
@@ -23,7 +23,3 @@ Pkg.instantiate()
 println("Instantiating package app")
 Pkg.activate(Base.source_dir() * "/app")
 Pkg.instantiate()
-
-println("Instantiating package docs")
-Pkg.activate(Base.source_dir() * "/docs")
-Pkg.instantiate()
