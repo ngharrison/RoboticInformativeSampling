c20b0398a0dda0f3af671ea6fe2697392a638ff9
Nicholas Harrison <nicholasharrison222@gmail.com>
2025-03-24 16:14:19 -0600

    misc: move exports to top of main module file

--- core/src/InformativeSampling.jl
+++ core/src/InformativeSampling.jl
@@ -1,10 +1,10 @@
 module InformativeSampling
 
+export SampleCosts, Samples, Missions
+
 # put all the code into the top namespace
 include("Samples.jl")
 include("SampleCosts.jl")
 include("Missions.jl")
-
-export SampleCosts, Samples, Missions
 
 end
